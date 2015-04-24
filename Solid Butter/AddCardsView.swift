dd446fcb8827d7f5fed67fc084473f73d5e16939
Tophatman100 <Tophatman100@gmail.com>
2015-04-23 21:26:44 -0500

    added Classes

---
+++ Solid Butter/AddCardsView.swift
@@ -0,0 +1,16 @@
+//
+//  AddCardsView.swift
+//  Solid Butter
+//
+//  Created by Dan  on 4/23/15.
+//  Copyright (c) 2015 Dan . All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AddCardsView : UIViewController {
+    
+    
+}
---
+++ Solid Butter/PlayView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayView.swift
+//  Solid Butter
+//
+//  Created by Dan  on 4/23/15.
+//  Copyright (c) 2015 Dan . All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class PlayView : UIViewController {
+    
+}
